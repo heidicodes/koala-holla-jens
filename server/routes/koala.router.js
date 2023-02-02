56f690237f2b08a0dc8c1b3d9737fbc288564a2f
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-02-02 14:11:28 -0600

    get router set up, get client set up on URL koala

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,17 +1,23 @@
-const express = require('express');
+const express = require("express");
 const koalaRouter = express.Router();
 
 // DB CONNECTION
 
-
 // GET
-
+koalaRouter.get("/", (req, res) => {
+  let queryText = `SELECT * FROM "koalas";`;
+  pool
+    .query(queryText)
+    .then((result) => res.send(result.rows))
+    .catch((err) => {
+      console.log(`Error making queryText: ${queryText}`, err);
+      res.sendStatus(500);
+    });
+});
 
 // POST
 
-
 // PUT
-
 
 // DELETE
 
